9b44792ea679abd18a1ad31e893e951a0230c6e2
Matthias Schneider <m.schneider.bam@googlemail.com>
2018-12-14 13:46:17 +0100

    no message

--- test.cs
+++ test.cs
@@ -3,11 +3,15 @@ public class TestClass
     public void TestMethod()
     {
         // Test
+        var a = 1;
         // Test
     }
 
     public void TestMethod(int x)
     {
+        // Test
+        // Test
+        // Test
         var a = x;
         // Test
     }
