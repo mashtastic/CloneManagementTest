353d7df8e0ac0283459afde2372170fc9b094d2d
Matthias Schneider <m.schneider.bam@googlemail.com>
2019-01-17 18:03:42 +0100

    no message

--- test.cs
+++ test.cs
@@ -8,10 +8,11 @@ public class TestClass
         }
     }
 
-    public void Fibo2(int a, int b, int n) {
-        if (n > 1) {
-            // Continue recursion
-            Fibo2(b, a + b, --n);
+    public void Fibo(int a, int b, int n)
+    {
+        if (n > 1)
+        {
+            Fibo(b, a + b, --n);
         }
     }
 }
